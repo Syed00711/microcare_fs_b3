bc9f63cf1dd4619e66efec33294a08bfdb278e1a
syed Hussain <syed.hussain@cerner.com>
2024-11-16 09:13:56 +0530

    updated

--- src/main/java/com/microcare/microservice/microcare_microservice/TestAPI.java
+++ src/main/java/com/microcare/microservice/microcare_microservice/TestAPI.java
@@ -1,12 +1,13 @@
 package com.microcare.microservice.microcare_microservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,6 +31,28 @@ public class TestAPI {
 	@GetMapping("/api/body")
 	public String checkBody(@RequestBody User user) {
 		return "RequestBody Response:"+user.toString();
+	}
+	
+	@PostMapping("/create")
+	public String createUser(@RequestBody User user) {
+		
+		
+		return "user created";
+	}
+	
+
+	@PutMapping("/update3")
+	public String updateUser(@RequestBody User user) {
+		
+		
+		return "user updated";
+	}
+	
+	@DeleteMapping("/delete")
+	public String deleteUser(@RequestParam int id) {
+		
+		
+		return "user deleted:"+id;
 	}
 
 }
